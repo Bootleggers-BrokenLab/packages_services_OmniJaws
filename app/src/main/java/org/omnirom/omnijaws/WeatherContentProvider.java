9947126788485703938540203fc1b3a79abe628a
tribual <tribual@web.de>
2024-10-14 21:16:27 +0200

    OmniJaws: hey, it's bootleg time again

--- app/src/main/java/org/omnirom/omnijaws/WeatherContentProvider.java
+++ app/src/main/java/org/omnirom/omnijaws/WeatherContentProvider.java
@@ -15,7 +15,7 @@
  */
 package org.omnirom.omnijaws;
 
-import com.android.internal.util.yaap.OmniJawsClient;
+import com.android.internal.util.bootleggers.OmniJawsClient;
 
 import org.omnirom.omnijaws.WeatherInfo.DayForecast;
 
